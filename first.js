cb5bc773486d9ccd944be768c24dc73a8e151614
Anant sahu <123002044+anantsahu21456@users.noreply.github.com>
2025-10-08 08:25:04 +0000

    update 2 nd luct.

--- first.js
+++ first.js
@@ -1,3 +1,56 @@
-console.log("hello world");
-console.log("learn with anant");
-console.log("learn with mone");
+// console.log("hello world");
+// console.log("learn with anant");
+// console.log("learn with mone");
+
+// let name = "anant";
+//  let name = "mone"
+// console.log(name);
+
+// var studentName = "megha"
+// var studentName = "anant"    // s_name, s_Name, s, 
+// var studentName = "mone"
+// console.log(studentName)
+
+// const heighScore = 300
+// heighScore = 301
+// console.log(heighScore);
+
+// let heighScore = 300
+// heighScore = 301
+// console.log(heighScore);
+
+// let score = 250
+// console.log(score);
+
+// const productMobilePrice = 5000
+// const addGstOnMobilePrice = 500
+// // const finalPrice = productMobilePrice + addGstOnMobilePrice
+// console.log("hey user this is your final price = ",  productMobilePrice + addGstOnMobilePrice);
+
+// let a = 40
+// let b = 9
+// console.log(a+b);
+// console.log(a-b);
+
+// let a = 5
+// let b = 2
+// console.log("line number 37 output = ", 5 / 2);
+// console.log("line number 38 output = " , 5 % 2);
+
+// let value1 = 299
+// let value2 = 399
+// console.log(value1 == value2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
